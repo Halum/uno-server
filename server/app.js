51421d533ff2290b4a180371ec0d292d52c35d6f
Sajjad Hossain <my.lost.word@gmail.com>
2019-02-12 18:40:25 +0600

    Initial structure

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const config = require('./config/config.js');
+const express = require('express');
+const http = require('http')
+const unoRouter = require('./uno')
+
+const app = express();
+
+app.use('/uno/', unoRouter);
+
+const server = http.createServer(app);
+const port = config.server.port;
+
+const onError = (error)=> {
+  if(error.syscall !== 'listen') throw error;
+
+  switch(error.code) {
+    case 'EACCES':
+      console.error(`${port} requires elevated privileges.`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${port} is already in use.`);
+      process.exit(1);
+      break;
+    default: throw error;
+  }
+}
+
+const onListen = () => {
+  const address = server.address();
+  const msg = typeof address === 'string' ? address : `port ${address.port}`;
+
+  console.log(`server is listening on ${msg}`);
+}
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListen);
+
+
+module.exports = server;
