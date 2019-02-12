528a26f49dc6b15110a6914ff7a606244c338bec
Sajjad Hossain <my.lost.word@gmail.com>
2019-02-12 18:45:17 +0600

    Make app.js smipler and move server to index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-require('./server/app');
+const app = require('./server/app');
+const config = require('./server/config/config');
+const http = require('http')
+
+const server = http.createServer(app);
+const port = config.server.port;
+
+const onError = (error)=> {
+  if(error.syscall !== 'listen') throw error;
+
+  switch(error.code) {
+    case 'EACCES':
+      console.error(`${port} requires elevated privileges.`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${port} is already in use.`);
+      process.exit(1);
+      break;
+    default: throw error;
+  }
+}
+
+const onListen = () => {
+  const address = server.address();
+  const msg = typeof address === 'string' ? address : `port ${address.port}`;
+
+  console.log(`server is listening on ${msg}`);
+}
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListen);
+
+module.exports = server;
--- server/app.js
+++ server/app.js
@@ -7,35 +7,4 @@ const app = express();
 
 app.use('/uno/', unoRouter);
 
-const server = http.createServer(app);
-const port = config.server.port;
-
-const onError = (error)=> {
-  if(error.syscall !== 'listen') throw error;
-
-  switch(error.code) {
-    case 'EACCES':
-      console.error(`${port} requires elevated privileges.`);
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(`${port} is already in use.`);
-      process.exit(1);
-      break;
-    default: throw error;
-  }
-}
-
-const onListen = () => {
-  const address = server.address();
-  const msg = typeof address === 'string' ? address : `port ${address.port}`;
-
-  console.log(`server is listening on ${msg}`);
-}
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListen);
-
-
-module.exports = server;
+module.exports = app;
