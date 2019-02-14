b428df9750c9501a6c2cc8bb8cdf21e4c858f865
Sajjad Hossain <my.lost.word@gmail.com>
2019-02-14 18:02:08 +0600

    Add demo.html to test logics

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 const unoRouter = require('./uno/uno.router');
 
 const app = express();
 
 app.use(express.json());
 app.use('/uno/', unoRouter);
+
+// this is temporary
+app.get('/', (req, res) => {
+  const demoFilePath = path.resolve(__dirname, '../', 'public/demo.html');
+  res.sendFile(demoFilePath);
+});
 
 module.exports = app;
