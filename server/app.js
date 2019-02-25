23d86bf575bb1dacb2914831c30fea06b367e5c8
Sajjad Hossain <my.lost.word@gmail.com>
2019-02-25 18:01:25 +0600

    Fix cors error while requested from UI

--- server/app.js
+++ server/app.js
@@ -4,6 +4,14 @@ const unoRouter = require('./uno/uno.router');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'POST, PUT,OPTIONS, DELETE, GET')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin')
+  res.header('Access-Control-Allow-Credentials', true)
+  next()
+})
+
 app.use(express.json());
 app.use('/uno/', unoRouter);
 
